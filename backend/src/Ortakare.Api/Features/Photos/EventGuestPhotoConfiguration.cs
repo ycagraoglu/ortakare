5ea0b1bee69077897a455c35814cc1383df95743
Yusuf Çağraoğlu <ycagraoglu@hotmail.com>
2026-07-14 22:22:13 +0300

    Configure guest photo persistence

---
+++ backend/src/Ortakare.Api/Features/Photos/EventGuestPhotoConfiguration.cs
@@ -0,0 +1,32 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ortakare.Api.Features.Events;
+using Ortakare.Api.Features.Participants;
+
+namespace Ortakare.Api.Features.Photos;
+
+public sealed class EventGuestPhotoConfiguration : IEntityTypeConfiguration<EventGuestPhoto>
+{
+    public void Configure(EntityTypeBuilder<EventGuestPhoto> builder)
+    {
+        builder.ToTable("EventGuestPhotos");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.StorageKey).HasMaxLength(500).IsRequired();
+        builder.Property(x => x.OriginalFileName).HasMaxLength(255).IsRequired();
+        builder.Property(x => x.ContentType).HasMaxLength(100).IsRequired();
+
+        builder.HasIndex(x => new { x.ParticipantId, x.ClientUploadId }).IsUnique();
+        builder.HasIndex(x => new { x.EventId, x.CreatedAtUtc });
+
+        builder.HasOne<Event>()
+            .WithMany()
+            .HasForeignKey(x => x.EventId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne<EventGuestParticipant>()
+            .WithMany()
+            .HasForeignKey(x => x.ParticipantId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
