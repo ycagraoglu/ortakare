1c125afd20c2cc0a8cbd40dab90f91305ddee615
Yusuf Çağraoğlu <ycagraoglu@hotmail.com>
2026-07-14 13:59:37 +0300

    Remove speculative domain base entity

--- backend/src/Ortakare.Domain/Common/BaseEntity.cs
+++
@@ -1,8 +0,0 @@
-namespace Ortakare.Domain.Common;
-
-public abstract class BaseEntity
-{
-    public Guid Id { get; set; }
-    public DateTime CreatedAtUtc { get; set; }
-    public DateTime? UpdatedAtUtc { get; set; }
-}
