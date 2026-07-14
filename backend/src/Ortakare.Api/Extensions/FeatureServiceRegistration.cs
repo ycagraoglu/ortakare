29191d299e22cee90d5f649053e777fc43f36957
Yusuf Çağraoğlu <ycagraoglu@hotmail.com>
2026-07-14 14:57:53 +0300

    Register create event services

--- backend/src/Ortakare.Api/Extensions/FeatureServiceRegistration.cs
+++ backend/src/Ortakare.Api/Extensions/FeatureServiceRegistration.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Identity;
 using Ortakare.Api.Features.Auth.Login;
 using Ortakare.Api.Features.Auth.Refresh;
 using Ortakare.Api.Features.Auth.Register;
+using Ortakare.Api.Features.Events.CreateEvent;
 using Ortakare.Api.Features.System.Health;
 using Ortakare.Api.Features.Users;
 using Ortakare.Api.Infrastructure.Authentication;
@@ -16,10 +17,13 @@ public static class FeatureServiceRegistration
         services.AddScoped<RegisterHandler>();
         services.AddScoped<LoginHandler>();
         services.AddScoped<RefreshHandler>();
+        services.AddScoped<CreateEventHandler>();
 
         services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
         services.AddSingleton<IAccessTokenService, JwtAccessTokenService>();
         services.AddSingleton<IRefreshTokenService, RefreshTokenService>();
+        services.AddHttpContextAccessor();
+        services.AddScoped<ICurrentUser, HttpCurrentUser>();
 
         return services;
     }
