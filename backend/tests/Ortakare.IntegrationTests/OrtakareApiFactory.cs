a1d53c6d0c8a9227f9b386ca09ebf798f06f1ac9
Yusuf Çağraoğlu <ycagraoglu@hotmail.com>
2026-07-14 22:26:23 +0300

    Use test object storage

--- backend/tests/Ortakare.IntegrationTests/OrtakareApiFactory.cs
+++ backend/tests/Ortakare.IntegrationTests/OrtakareApiFactory.cs
@@ -3,21 +3,27 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Ortakare.Api.Infrastructure.ObjectStorage;
 using Ortakare.Api.Infrastructure.Persistence;
 
 namespace Ortakare.IntegrationTests;
 
 public sealed class OrtakareApiFactory : WebApplicationFactory<Program>
 {
+    public TestObjectStorageService ObjectStorage { get; } = new();
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.ConfigureServices(services =>
         {
             services.RemoveAll<DbContextOptions<OrtakareDbContext>>();
             services.RemoveAll<OrtakareDbContext>();
+            services.RemoveAll<IObjectStorageService>();
 
             services.AddDbContext<OrtakareDbContext>(options =>
                 options.UseInMemoryDatabase($"ortakare-tests-{Guid.NewGuid():N}"));
+
+            services.AddSingleton<IObjectStorageService>(ObjectStorage);
         });
     }
 }
