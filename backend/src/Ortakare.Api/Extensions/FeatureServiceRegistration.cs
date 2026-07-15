7f94bc54e8f291a684e18f7c0204adca3cd4cbec
Yusuf Çağraoğlu <ycagraoglu@hotmail.com>
2026-07-15 22:42:45 +0300

    Register gallery export deletion handler

--- backend/src/Ortakare.Api/Extensions/FeatureServiceRegistration.cs
+++ backend/src/Ortakare.Api/Extensions/FeatureServiceRegistration.cs
@@ -12,6 +12,7 @@ using Ortakare.Api.Features.Events.RegenerateGalleryToken;
 using Ortakare.Api.Features.Events.ReopenEvent;
 using Ortakare.Api.Features.Events.UpdateEvent;
 using Ortakare.Api.Features.GalleryExports.CreateGalleryExport;
+using Ortakare.Api.Features.GalleryExports.DeleteGalleryExport;
 using Ortakare.Api.Features.GalleryExports.GetEventExports;
 using Ortakare.Api.Features.GalleryExports.GetGalleryExport;
 using Ortakare.Api.Features.GalleryExports.RetryFailedGalleryExport;
@@ -66,6 +67,7 @@ public static class FeatureServiceRegistration
         services.AddScoped<GetGalleryExportHandler>();
         services.AddScoped<GetEventExportsHandler>();
         services.AddScoped<RetryFailedGalleryExportHandler>();
+        services.AddScoped<DeleteGalleryExportHandler>();
         services.AddScoped<BuildGalleryExportJob>();
 
         services.AddScoped<IPasswordHasher<User>, PasswordHasher<User>>();
