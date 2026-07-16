5271647036e30560e7301aec931e0765c1ece4bd
Yusuf Çağraoğlu <ycagraoglu@hotmail.com>
2026-07-16 16:14:19 +0300

    Configure in-app notifications

---
+++ backend/src/Ortakare.Api/Features/Notifications/NotificationConfiguration.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Ortakare.Api.Features.Events;
+using Ortakare.Api.Features.Users;
+
+namespace Ortakare.Api.Features.Notifications;
+
+public sealed class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+{
+    public void Configure(EntityTypeBuilder<Notification> builder)
+    {
+        builder.ToTable("Notifications");
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Type).HasMaxLength(80).IsRequired();
+        builder.Property(x => x.Title).HasMaxLength(160).IsRequired();
+        builder.Property(x => x.Message).HasMaxLength(500).IsRequired();
+        builder.Property(x => x.DataJson).HasColumnType("jsonb");
+
+        builder.HasIndex(x => new { x.OwnerUserId, x.ReadAtUtc, x.CreatedAtUtc });
+        builder.HasIndex(x => new { x.EventId, x.CreatedAtUtc });
+
+        builder.HasOne<User>()
+            .WithMany()
+            .HasForeignKey(x => x.OwnerUserId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne<Event>()
+            .WithMany()
+            .HasForeignKey(x => x.EventId)
+            .OnDelete(DeleteBehavior.SetNull);
+    }
+}
