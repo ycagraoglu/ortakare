271786ba52e31e96b0480f54b63e2d726b9b7068
Yusuf Çağraoğlu <ycagraoglu@hotmail.com>
2026-07-18 01:29:42 +0300

    Remove unused gallery export options

--- backend/src/Ortakare.Api/Features/GalleryExports/GalleryExportOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace Ortakare.Api.Features.GalleryExports;
-
-public sealed class GalleryExportOptions
-{
-    public const string SectionName = "GalleryExport";
-
-    public int RetentionDays { get; init; } = 7;
-}
