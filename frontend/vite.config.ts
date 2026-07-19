6d578cc511654338edb391c8cb7d110124bdbde7
Yusuf Çağraoğlu <ycagraoglu@hotmail.com>
2026-07-19 13:42:11 +0300

    chore(frontend): add Vite configuration

---
+++ frontend/vite.config.ts
@@ -0,0 +1,16 @@
+import { fileURLToPath, URL } from "node:url";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  build: {
+    target: "es2022",
+    sourcemap: true,
+  },
+});
