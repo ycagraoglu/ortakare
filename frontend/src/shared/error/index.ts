efec4a2963e771f3511109a104ef400a079bba94
Yusuf Çağraoğlu <ycagraoglu@hotmail.com>
2026-07-21 15:55:45 +0300

    Export global error utilities

---
+++ frontend/src/shared/error/index.ts
@@ -0,0 +1,5 @@
+import "@/shared/error/error.css";
+
+export * from "@/shared/error/ApplicationErrorBoundary";
+export * from "@/shared/error/GlobalErrorFallback";
+export * from "@/shared/error/error-utils";
