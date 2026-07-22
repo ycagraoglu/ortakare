547d80726826a85ad32a25595160dac1adb6746d
Yusuf Çağraoğlu <ycagraoglu@hotmail.com>
2026-07-22 16:21:30 +0300

    feat(frontend): export accessibility primitives

---
+++ frontend/src/shared/accessibility/index.ts
@@ -0,0 +1,3 @@
+export { AccessibleDialog } from "@/shared/accessibility/AccessibleDialog";
+export { RouteAccessibility } from "@/shared/accessibility/RouteAccessibility";
+export { SkipLink } from "@/shared/accessibility/SkipLink";
